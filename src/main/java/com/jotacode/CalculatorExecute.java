1a7c8185da602893af22a27ab4060f99de8c77db
JotaValla <jimmy.valladares@epn.edu.ec>
2024-10-22 16:43:04 -0500

    Adding calculator execute class. Adding calculator JAR

---
+++ src/main/java/com/jotacode/CalculatorExecute.java
@@ -0,0 +1,17 @@
+package com.jotacode;
+
+public class CalculatorExecute {
+
+    public static void main(String[] args) {
+
+        System.out.println("CalculatorExecute");
+
+        Calculator c = new Calculator();
+        int addition = c.addition(2, 4);
+        System.out.println("c.addition(2, 4) = " + addition);
+
+        int subtraction = c.subtraction(4, 2);
+        System.out.println("c.subtraction(4, 2) = " + subtraction);
+
+    }
+}
